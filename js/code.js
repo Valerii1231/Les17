29ebfbd0b87e09bf2b7291cf1d9dbbc2db5b2e55
Valerii1231 <vm010166@gmail.com>
2022-12-16 20:24:40 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,13 @@
+console.log('You can ignore single and doubl quotes like this: \\\'\\\' \\\" \\\"');
+
+alert ( 'Please enter your data');
+let userName = prompt("Enter your name?", 'Name');
+alert(`Your name is: ${userName}`);
+let surName = prompt("Enter your surname?", 'Surname');
+alert(`Your surname is: ${userName} ${surName}`);
+let age = prompt("How old are you?", 'Age');
+alert(`Your name is: ${userName} ${surName}, your age is: ${age} `);
+alert ('Are you admin?');
+alert(`Your name is: ${userName} ${surName}, your age is: ${age}, admin status: true`);
+
+
